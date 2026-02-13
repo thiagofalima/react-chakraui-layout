932ca4bb0fc039eb011e741c2c65df1ad1cbd9f5
thiagofalima <thiagofalima@hotmail.com>
2026-02-13 17:51:05 -0300

    feat: Update login function with async behaviour

--- src/components/login.test.tsx
+++ src/components/login.test.tsx
@@ -5,16 +5,21 @@ describe('login', () => {
 
     const mockAlert = vi.fn();
     window.alert = mockAlert
-    const mockEmail = 'thiago.lima@senai.com'
+    const mockEmail = 'thiago@senai.com'
 
-    it('Deve exibir um alert com boas vindas', () => {
-        login(mockEmail)
+    it('Deve exibir um alert com boas vindas', async () => {
+        await login(mockEmail)
         expect(mockAlert).toHaveBeenCalledWith(`Bem-vindo ${mockEmail}!`)
     })
 
-    it('Não deve dar apenas boas vindas', () => {
-        login(mockEmail)
-        expect(mockAlert).toHaveBeenCalledWith(`Bem-vindo`)
+    it('Não deve dar apenas boas vindas', async () => {
+        await login(mockEmail)
+        expect(mockAlert).not.toHaveBeenCalledWith(`Bem-vindo`)
+    })
+
+    it('Deve exibir um erro caso o email seja inválido', async () => {
+        await login('email@invalido.com')
+        expect(mockAlert).toHaveBeenCalledWith('Email inválido')
     })
 
 })
