561cfade85054b82ae224c298cf94fe9ec96ce99
thiagofalima <thiagofalima@hotmail.com>
2026-02-23 21:07:03 -0300

    feat: reate AppContext separated component

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import { AccountPage } from "./pages/AccountPage"
 import Layout from "./components/Layout";
-import { createContext } from "react";
-
-interface IAppContext {
-  user: string
-}
-
-const AppContext = createContext({} as IAppContext)
-
-const AppContextProvider = ({ children }: any) => {
-  const user = 'thiago'
-
-  return (
-    <AppContext.Provider value={{ user }}>
-      { children }
-    </AppContext.Provider>
-  )
-}
+import { AppContextProvider } from "./components/AppContext";
 
 function App() {
 
---
+++ src/components/AppContext.tsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+interface IAppContext {
+  user: string
+}
+
+export const AppContext = createContext({} as IAppContext)
+
+export const AppContextProvider = ({ children }: any) => {
+  const user = 'thiago'
+
+  return (
+    <AppContext.Provider value={{ user }}>
+      { children }
+    </AppContext.Provider>
+  )
+}
